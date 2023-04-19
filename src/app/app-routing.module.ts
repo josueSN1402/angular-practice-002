15e4809fc926ecf404380f2a6960187799ccede2
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-04-19 01:00:10 -0500

    Inicializando componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoursesComponent } from './courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'courses',
+    component: CoursesComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
