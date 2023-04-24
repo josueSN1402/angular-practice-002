99880934c3e761702d04b5ca636e42ba2be6723a
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-04-24 16:05:50 -0500

    Remove components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseAddComponent } from './course-add/course-add.component';
-import { CourseAddReactiveComponent } from './course-add-reactive/course-add-reactive.component';
-import { CourseEditComponent } from './course-edit/course-edit.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent,
-    CourseAddComponent,
-    CourseAddReactiveComponent,
-    CourseEditComponent,
-  ],
+  declarations: [AppComponent, CoursesComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
