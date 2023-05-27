c024b87a058ed81b6679404a76200e0fb9facdca
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-05-27 02:56:36 -0500

    Acceder a instancia del form

--- src/app/course-add/course-add.component.ts
+++ src/app/course-add/course-add.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Curso } from '../curso';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'pa-course-add',
@@ -10,4 +12,27 @@ export class CourseAddComponent {
   precio!: number;
   urlImagen: string = '';
   descripcion: string = '';
+
+  model: Curso = {
+    id: 0,
+    name: '',
+    description: '',
+    startDate: '',
+    price: 0,
+    rating: 0,
+    imageUrl: '',
+  };
+
+  @ViewChild('formAdd', { static: false })
+  form!: FormControl;
+
+  onSubmit() {
+    console.log('onSubmit', this.form);
+    if (this.form.valid) {
+      // Enviar el modelo a un servidor
+      console.log('modelo', this.model);
+      // Reinicializar el formulario
+      this.form.reset();
+    }
+  }
 }
