5aa1d727391e1edeeaecae14cbebdbb26cf6f010
Josue Sanchez Nima <josue.sanchez.n1402@hotmail.com>
2023-05-27 02:32:03 -0500

    Validación de form

--- src/app/course-add/course-add.component.ts
+++ src/app/course-add/course-add.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pa-course-add',
   templateUrl: './course-add.component.html',
-  styleUrls: ['./course-add.component.scss']
+  styleUrls: ['./course-add.component.scss'],
 })
 export class CourseAddComponent {
-
+  nombre: string = '';
+  precio!: number;
+  urlImagen: string = '';
+  descripcion: string = '';
 }
